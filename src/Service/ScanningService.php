d0830af9b94388d8c4b5e5a11ebf7fb0ed37e41b
Alsciende <bertolini.cedric@me.com>
2018-01-28 19:57:16 +0100

    fix logger and add more logs

--- src/Service/ScanningService.php
+++ src/Service/ScanningService.php
@@ -7,6 +7,7 @@ use Alsciende\SerializerBundle\Annotation\Skizzle;
 use Alsciende\SerializerBundle\Model\Source;
 use Alsciende\SerializerBundle\Util\SourceOrderingService;
 use Doctrine\Common\Annotations\Reader;
+use Psr\Log\LoggerInterface;
 
 /**
  * This Service finds all Sources declared in the application
@@ -21,6 +22,9 @@ class ScanningService
     /** @var Reader $reader */
     private $reader;
 
+    /** @var LoggerInterface $logger */
+    private $logger;
+
     public function __construct (
         MetadataService $metadataService,
         Reader $reader
@@ -31,17 +35,40 @@ class ScanningService
     }
 
     /**
+     * @param LoggerInterface $logger
+     *
+     * @return $this
+     */
+    public function setLogger (LoggerInterface $logger): self
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+
+    /**
      * @return Source[]
      */
     public function findSources (): array
     {
         $sources = [];
 
-        foreach ($this->metadataService->getAllManagedClassNames() as $className) {
+        $allManagedClassNames = $this->metadataService->getAllManagedClassNames();
+        if($this->logger instanceof LoggerInterface) {
+            $this->logger->notice('Classes discovered through ORM Metadata', $allManagedClassNames);
+        }
+
+        foreach ($allManagedClassNames as $className) {
             $source = $this->buildFromClass($className);
             if ($source instanceof Source) {
                 $sources[] = $source;
             }
+        }
+
+        if($this->logger instanceof LoggerInterface) {
+            $this->logger->notice('List of Skizzle sources', array_map(function (Source $source) {
+                return $source->getClassName();
+            }, $sources));
         }
 
         $ordering = new SourceOrderingService($this->metadataService);
